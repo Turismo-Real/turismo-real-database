ccf5b06a7a13720de1bc4e6423a85929c3f9cba8
enzo.norese <enzo.norese@prodigio.tech>
2021-10-06 20:45:43 -0300

    Se modifica SP listar usuarios

--- StoredProcedures/Usuario/sp_obten_usuarios.sql
+++ StoredProcedures/Usuario/sp_obten_usuarios.sql
@@ -2,12 +2,18 @@
 create or replace procedure sp_obten_usuarios(usuarios out sys_refcursor)
 is begin
     open usuarios for
-        select numrut,dvrut,pnombre,snombre,apepat,apemat,
-            fec_nac,correo,telefono_movil,telefono_fijo,
-            genero,pais,tipo
+        select numrut, dvrut,
+            pnombre, snombre, apepat, apemat,
+            fec_nac, correo,
+            telefono_movil, telefono_fijo,
+            genero, pais, tipo, region, comuna,
+            calle, numero, depto, casa
         from usuario join genero using(id_genero)
         join pais using(id_pais)
-        join tipo_usuario using(id_tipo);
+        join tipo_usuario using(id_tipo)
+        join direccion using(numrut)
+        join comuna using(id_comuna)
+        join region using(id_region);
 end;
 
 -- PRUEBA SP
@@ -27,11 +33,18 @@ declare
     genero turismo_real.genero.genero%type;
     pais turismo_real.pais.pais%type;
     tipo turismo_real.tipo_usuario.tipo%type;
+    region turismo_real.region.region%type;
+    comuna turismo_real.comuna.comuna%type;
+    calle turismo_real.direccion.calle%type;
+    numero turismo_real.direccion.numero%type;
+    depto turismo_real.direccion.depto%type;
+    casa turismo_real.direccion.casa%type;
 begin
     sp_obten_usuarios(c_usuario);
 
     loop fetch c_usuario into numrut,dvrut,pnombre,snombre,apepat,apemat,
-        fec_nac,correo,telefono_movil,telefono_fijo,genero,pais,tipo;
+        fec_nac,correo,telefono_movil,telefono_fijo,genero,pais,tipo,
+        region,comuna,calle,numero,depto,casa;
         exit when c_usuario%notfound;
 
         dbms_output.put_line('RUT: '||numrut||'-'||dvrut);
@@ -43,6 +56,12 @@ begin
         dbms_output.put_line('Genero: '||genero);
         dbms_output.put_line('Pais: '||pais);
         dbms_output.put_line('Perfil: '||tipo);
+        dbms_output.put_line('Region: '||region);
+        dbms_output.put_line('Comuna: '||comuna);
+        dbms_output.put_line('Calle: '||calle);
+        dbms_output.put_line('Numero: '||numero);
+        dbms_output.put_line('Depto: '||depto);
+        dbms_output.put_line('Casa: '||casa);
         dbms_output.put_line('-----------------------------');
         dbms_output.put_line('');
     end loop;

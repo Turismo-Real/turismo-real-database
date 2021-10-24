a7468a2974e9cc57f97e3dfe9374db95b3224328
enzo.norese <enzo.norese@prodigio.tech>
2021-10-23 22:08:10 -0300

    Add: SP agregar reserva a script general

--- 3. turismo_real_objects.sql
+++ 3. turismo_real_objects.sql
@@ -40,6 +40,8 @@ drop procedure sp_obten_paises;
 drop procedure sp_obten_reservas;
 drop procedure sp_obten_reserva_id;
 drop procedure sp_eliminar_reserva;
+drop procedure sp_agregar_reserva;
+drop procedure sp_obten_fechas_reservadas;
 
 /
 
@@ -939,4 +941,91 @@ exception
 end;
 
 /
+
+-- SP AGREGAR RESERVA
+create or replace procedure sp_agregar_reserva(
+    fec_desde_r in varchar,
+    fec_hasta_r in varchar,
+    id_usuario_r in number,
+    id_departamento_r in number,
+    saved out number
+) is
+    reserva_id turismo_real.reserva.id_reserva%type;
+    fec_desde_in turismo_real.reserva.fec_desde%type;
+    fec_hasta_in turismo_real.reserva.fec_hasta%type;
+    estado_id turismo_real.estado_depto.estado%type;
+    tope_reservas number;
+    dias_arriendo number;
+    valor_total_arriendo number;
+    cliente_id number;
+    depto_id number;
+    tope_reserva number;
+begin
+    -- comprobar existencia de cliente
+    cliente_id := fn_obten_cliente(id_usuario_r);
+    -- comprobar existencia de departamento
+    depto_id := fn_obten_depto(id_departamento_r);   
+    
+    if cliente_id > 0 and depto_id > 0 then
+        -- convertir str fecha entrada a tipo fecha
+        fec_desde_in := to_date(fec_desde_r);
+        fec_hasta_in := to_date(fec_hasta_r);
+        -- obtener topes de fecha con otras reservas
+        tope_reserva := fn_obten_tope_reserva(depto_id, fec_desde_in, fec_hasta_in);
+        -- obtener dias de arriendo
+        dias_arriendo := fn_calcular_dias_arriendo(fec_desde_in, fec_hasta_in);
+        
+        if tope_reserva = 0 and dias_arriendo <= 30 then
+            -- obtener valor arriendo
+            valor_total_arriendo := fn_caclular_total_arriendo(depto_id, dias_arriendo);
+            -- obtener id estado reserva (default CARGADA)
+            estado_id := fn_obten_id_estado_reserva('CARGADA');
+            -- generar id reserva
+            reserva_id := seq_reserva.nextval;
+            
+            -- crear nueva reserva
+            insert into reserva(id_reserva,fechora_reserva,fec_desde,fec_hasta,valor_arriendo,id_estado,id_departamento,id_usuario)
+                values(reserva_id,sysdate,fec_desde_in,fec_hasta_in,valor_total_arriendo,estado_id,depto_id,cliente_id);
+
+            commit;            
+            saved := reserva_id; -- OK: RETORNA ID RESERVA
+        else
+            if tope_reserva > 0 then
+                saved := -3; -- ERROR: YA EXISTE UNA RESERVA EN TAL FECHA
+            elsif dias_arriendo > 30 then
+                saved := -4; -- ERROR: 30 DIAS MAXIMO RESERVA
+            end if;
+        
+        end if;       
+    else
+        if cliente_id = 0 then
+            saved := -1; -- ERROR: CLIENTE NO EXISTE 
+        elsif depto_id = 0 then
+            saved := -2; -- ERROR: DEPTO NO EXISTE
+        end if;
+    end if;
+exception
+    when others then
+        saved := 0; -- ERROR AL AGREGAR RESERVA
+end;
+
+/
+
+-- SP OBTEN FECHAS RESERVADAS
+create or replace procedure sp_obten_fechas_reservadas(depto_id in number, fechas_reservadas out sys_refcursor)
+is begin
+    -- obtener fechas reservadas
+    -- maximo 30 dias por reserva
+    open fechas_reservadas for
+        select
+            to_char(fec_desde,'dd/mm/yyyy') as fec_desde,
+            to_char(fec_hasta,'dd/mm/yyyy') as fec_hasta
+        from reserva
+        where id_departamento = depto_id
+        and fec_desde >= sysdate-35
+        order by fec_desde, fec_hasta;
+end;
+
+/
+
 commit;

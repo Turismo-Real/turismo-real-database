c4918d6ccac0514b9102d6e23e4e224fda74b0a7
Enzo Norese <enzo.norese@gmail.com>
2021-10-24 21:18:04 -0300

    Add: SP check in a script general

--- 3. turismo_real_objects.sql
+++ 3. turismo_real_objects.sql
@@ -1046,4 +1046,41 @@ end;
 
 /
 
+-- SP CHECK IN
+create or replace procedure sp_check_in(
+    reserva_id in number,
+    conforme in number,
+    estado in varchar,
+    check_in out number
+)
+is
+    id_r turismo_real.reserva.id_reserva%type;
+    estado_id turismo_real.estado_depto.id_estado%type;
+begin
+    -- comprobar existencia de reserva
+    select id_reserva into id_r
+    from reserva
+    where id_reserva = reserva_id;
+
+    -- id de estado (check in)
+    estado_id := fn_obten_id_estado_reserva('CHECK IN');
+
+    -- ingresar check in
+    update reserva
+        set fechora_checkin = sysdate,
+            checkin_conforme = conforme,
+            estado_checkin = estado
+    where id_reserva = id_r;
+
+    commit;
+    check_in := 1; -- check in ingresado
+exception
+    when no_data_found then
+        check_in := -1; -- RESERVA NO EXISTE
+    when others then
+        check_in := 0; -- ERROR EN CHECK IN
+end;
+
+/
+
 commit;

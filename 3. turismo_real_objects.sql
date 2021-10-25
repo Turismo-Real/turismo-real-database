66aa3e3e95b9accefa8aff9fcf0d9af330a20eb3
Enzo Norese <enzo.norese@gmail.com>
2021-10-24 21:18:41 -0300

    Add: SP check out a script general

--- 3. turismo_real_objects.sql
+++ 3. turismo_real_objects.sql
@@ -1083,4 +1083,41 @@ end;
 
 /
 
+-- SP CHECK OUT
+create or replace procedure sp_check_out(
+    reserva_id in number,
+    conforme in number,
+    estado in varchar,
+    check_out out number
+)
+is
+    id_r turismo_real.reserva.id_reserva%type;
+    estado_id turismo_real.estado_depto.id_estado%type;
+begin
+    -- comprobar existencia de reserva
+    select id_reserva into id_r
+    from reserva
+    where id_reserva = reserva_id;
+
+    -- id de estado (check in)
+    estado_id := fn_obten_id_estado_reserva('CHECK OUT');
+
+    -- ingresar check out
+    update reserva
+        set fechora_checkout = sysdate,
+            checkout_conforme = conforme,
+            estado_checkout = estado
+    where id_reserva = id_r;
+
+    commit;
+    check_out := 1; -- check in ingresado
+exception
+    when no_data_found then
+        check_out := -1; -- RESERVA NO EXISTE
+    when others then
+        check_out := 0; -- ERROR EN CHECK IN
+end;
+
+/
+
 commit;
